56ead61bca4da41ea150bc305e5992cb888bf29c
Jonathan Mak <jchm2@gwash.cl.cam.ac.uk>
2009-12-16 12:52:29 +0000

    New options. Not generating pdfs any longer

--- postprocess/embla.sh
+++ postprocess/embla.sh
@@ -1,45 +1,34 @@
-#! /bin/sh
+#!/bin/sh
 
-eval EXEC=\$$#
-EMBLA_BIN=/home/jchm2/temp/embla-bin
-EMBLA_HOME=/home/jchm2/Work/embla
-OUTPUT=$EXEC.out
-TRACE_DUMP=$EXEC.trace_dump
-DEPLIST_TEX=$EXEC.deplist.tex
-DEPGRAPH_TEX=$EXEC.depgraph.tex
+CLIENT_PROG=$*
+BASENAME=`basename $1`
+EMBLA_BIN=/home/jchm2/gscratch/embla-bin
+SCRIPT_DIR=/home/jchm2/Work/embla/postprocess
+CREATE_CTLDEPS=$SCRIPT_DIR/cfa.sh
+CREATE_DATADEPS="awk -f $SCRIPT_DIR/datadeps.awk"
+CREATE_LOOPS=$SCRIPT_DIR/loops.sh
+HIDDEN_FUNC_FILE=$EMBLA_BIN/embla.hidden-funcs
+N_TRACE_RECS=80000000
+TRACE_FILE=$BASENAME.trace
+EDGES_FILE=$BASENAME.edges
+LOOP_FILE=$BASENAME.loops
+DEPS_FILE=$BASENAME.deps
+TRACE_FILE_LOOP=$BASENAME.ltrace
+EDGES_FILE_LOOP=$BASENAME.ledges
+DEPS_FILE_LOOP=$BASENAME.ldeps
 
-#echo $EXEC
+# Dynamic data deps, ILP, dynamic control deps, early spawns, no loops
+# Used for generating static deps file
+eval "$EMBLA_BIN/bin/valgrind --tool=embla --hidden-func-file=$HIDDEN_FUNC_FILE --n_trace_recs=$N_TRACE_RECS --loop-file=/dev/null --trace-file=$TRACE_FILE --edge-file=$EDGES_FILE $CLIENT_PROG 1>&2"
 
-$EMBLA_BIN/bin/valgrind --tool=embla --trace-file=$EXEC.trace $* >$OUTPUT 2>&1
-grep "^[0-9]\+: " $OUTPUT >$TRACE_DUMP
+# Create no-loop depfile
+$CREATE_CTLDEPS $EDGES_FILE >$DEPS_FILE
+$CREATE_DATADEPS $TRACE_FILE >>$DEPS_FILE
+# Create loop file
+$CREATE_LOOPS $EDGES_FILE >$LOOP_FILE
 
-cat <<EOF >$DEPLIST_TEX
-\documentclass{article}
-\usepackage{color}
-\usepackage{epic}
-\usepackage{verbdef}
-\begin{document}
-EOF
+eval "$EMBLA_BIN/bin/valgrind --tool=embla --hidden-func-file=$HIDDEN_FUNC_FILE --n_trace_recs=$N_TRACE_RECS --loop-file=$LOOP_FILE --trace-file=$TRACE_FILE_LOOP --edge-file=$EDGES_FILE_LOOP $CLIENT_PROG 1>&2"
 
-cat <<EOF >$DEPGRAPH_TEX
-\documentclass{article}
-\usepackage{color}
-\usepackage{epic}
-\usepackage{verbdef}
-\begin{document}
-EOF
-
-awk -f $EMBLA_HOME/postprocess/deplist.awk $EXEC.c $EXEC.trace >> $DEPLIST_TEX
-awk -f $EMBLA_HOME/postprocess/depgraph.awk $EXEC.c $EXEC.trace >> $DEPGRAPH_TEX
-
-cat <<EOF >>$DEPLIST_TEX
-\end{document}
-EOF
-
-cat <<EOF >>$DEPGRAPH_TEX
-\end{document}
-EOF
-
-pdflatex $DEPLIST_TEX
-pdflatex $DEPGRAPH_TEX
-
+# Create no-loop depfile
+$CREATE_CTLDEPS $EDGES_FILE_LOOP >$DEPS_FILE_LOOP
+$CREATE_DATADEPS $TRACE_FILE_LOOP >>$DEPS_FILE_LOOP
